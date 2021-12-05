48082813e17008a023ade036a4b1a35a81ae2622
wodydtns <wodydtns@naver.com>
2021-12-05 10:01:25 +0900

    내용 : 패키지 에러 정리, 트랜잭션 result type enum 추가

---
+++ src/main/java/sophist/common/ResultType.java
@@ -0,0 +1,6 @@
+package sophist.common;
+
+// SUCCESS : 성공 / FAIL : 실패
+public enum ResultType {
+	SUCCESS,FAIL
+}
--- src/main/java/sophist/log/controller/LogInfoController.java
+++ src/main/java/sophist/log/controller/LogInfoController.java
@@ -2,29 +2,45 @@ package sophist.log.controller;
 
 import javax.annotation.Resource;
 
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import sophist.log.model.SopiIpLog;
 import sophist.log.model.SopiSearchLog;
 import sophist.log.service.LogInfoService;
 
+
+@RestController
 public class LogInfoController {
 	
 	@Resource
 	private LogInfoService logInfoService;
 	
-	@GetMapping
+	@GetMapping(value = "/sophist/ipLog")
 	public Page<SopiIpLog> selectIpLogList(Pageable pageable) throws Throwable{
 		return logInfoService.selectIpLogList(pageable);
 		
 	}
 	
-	@GetMapping
+	@GetMapping(value="/sophist/searchLog")
 	public Page<SopiSearchLog> selectSearchLogList(Pageable pageable) throws Throwable{
 		return logInfoService.selectSearchLogList(pageable);
-		
 	}
+	
+	@PostMapping(value = "/sophist/saveLog")
+	public void insertSearchLog(SopiSearchLog sopiSearchLog) throws Throwable{
+		logInfoService.insertSearchLog(sopiSearchLog);
+	}
+	
+	@GetMapping(value="/sophist/searchLogDetail")
+	public SopiSearchLog selectSearchLogDetail(String searchCd) throws Throwable{
+		return logInfoService.selectSearchLogDetail(searchCd);
+	}
+	
+	
 
 }
