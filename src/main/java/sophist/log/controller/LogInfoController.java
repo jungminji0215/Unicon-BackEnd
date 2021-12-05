4b277ac7220134fa231ac23a505a2a001b6b9383
wodydtns <wodydtns@naver.com>
2021-12-05 09:08:33 +0900

    내용 : select,insert searchlog 추가

--- src/main/java/sophist/log/controller/LogInfoController.java
+++ src/main/java/sophist/log/controller/LogInfoController.java
@@ -1,5 +1,30 @@
 package sophist.log.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import sophist.log.model.SopiIpLog;
+import sophist.log.model.SopiSearchLog;
+import sophist.log.service.LogInfoService;
+
 public class LogInfoController {
+	
+	@Resource
+	private LogInfoService logInfoService;
+	
+	@GetMapping
+	public Page<SopiIpLog> selectIpLogList(Pageable pageable) throws Throwable{
+		return logInfoService.selectIpLogList(pageable);
+		
+	}
+	
+	@GetMapping
+	public Page<SopiSearchLog> selectSearchLogList(Pageable pageable) throws Throwable{
+		return logInfoService.selectSearchLogList(pageable);
+		
+	}
 
 }
