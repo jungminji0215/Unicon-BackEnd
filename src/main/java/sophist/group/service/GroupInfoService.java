311c4fa1782bb4dc964b35d0001e3087569de4a5
wodydtns <wodydtns@naver.com>
2021-12-12 09:14:39 +0900

    내용 : group info join query 구현

--- src/main/java/sophist/group/service/GroupInfoService.java
+++ src/main/java/sophist/group/service/GroupInfoService.java
@@ -1,5 +1,16 @@
 package sophist.group.service;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import sophist.group.model.SopiGroupMaster;
+
+
 public interface GroupInfoService {
 
+	public List<SopiGroupMaster> findAllWithFileUsingJoin();
+	
 }
--- src/main/java/sophist/group/service/impl/GroupInfoServiceImpl.java
+++ src/main/java/sophist/group/service/impl/GroupInfoServiceImpl.java
@@ -1,7 +1,28 @@
 package sophist.group.service.impl;
 
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import sophist.group.model.SopiGroupMaster;
+import sophist.group.repository.GroupRepository;
 import sophist.group.service.GroupInfoService;
 
+@Service
 public class GroupInfoServiceImpl implements GroupInfoService{
+	
+	@Resource
+	private GroupRepository groupRepository;
+
+	@Override
+	public List<SopiGroupMaster> findAllWithFileUsingJoin() {
+		return groupRepository.findAllWithFileUsingJoin();
+	}
+
 
 }
