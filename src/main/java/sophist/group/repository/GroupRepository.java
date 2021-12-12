8e8ee2dd99a24317e028848409c9395f878dd358
wodydtns <wodydtns@naver.com>
2021-12-12 09:33:35 +0900

    쿼리 수정

--- src/main/java/sophist/group/repository/GroupRepository.java
+++ src/main/java/sophist/group/repository/GroupRepository.java
@@ -5,11 +5,12 @@ import sophist.group.model.SopiGroupMaster;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface GroupRepository extends JpaRepository<SopiGroupMaster,String>{
 	
-	@Query("SELECT gm FROM SopiGroupMaster gm join fetch gm.sopiFileMaster inner join SopiFileMaster fm ON fm.fileCd=gm.fileCd")
-	public List<SopiGroupMaster> findAllWithFileUsingJoin();
+	@Query("SELECT gm,fm FROM SopiGroupMaster gm join fetch gm.sopiFileMaster inner join SopiFileDetail fm ON fm.fileCd=gm.fileCd")
+	public List<SopiGroupMaster> findAllWithFileUsingJoin(Pageable pageable);
 }
