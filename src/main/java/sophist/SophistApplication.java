7f5f15cb0e4a06bb79c00b6ed1a6cffcd2201e3a
minji <wjdalswl0215@gmail.com>
2021-12-21 22:23:47 +0900

    내용:로그인 구현

--- src/main/java/sophist/SophistApplication.java
+++ src/main/java/sophist/SophistApplication.java
@@ -2,8 +2,11 @@ package sophist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+// (exclude= {UserDetailsServiceAutoConfiguration.class}는 비번 암호화를 위해 
+// 스프링 시큐리티 사용 시, 시큐리티 로그인 화면으로 가는거에 대한 비번이 콘솔에 안뜨게..?
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 public class SophistApplication {
 
 	public static void main(String[] args) {
--- src/main/java/sophist/group/service/GroupInfoService.java
+++ src/main/java/sophist/group/service/GroupInfoService.java
@@ -1,13 +1,8 @@
 package sophist.group.service;
 
-
 import java.util.List;
-
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
 import sophist.group.model.SopiGroupMaster;
-
 
 public interface GroupInfoService {
 
